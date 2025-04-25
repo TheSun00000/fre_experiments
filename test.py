e9a896b40c8c501abb2ea3cc71bfda58db381639
TheSun00000 <jn_bendib@esi.dz>
2025-04-25 16:16:14 +0200

    moving old files to archive - adding test script

--- test.py
+++ test.py
@@ -22,6 +22,14 @@ device
 print('device:', device)
 
 
+REPLAY_BUFFER_FILE = 'models/new_states_buffer_7.pth'
+FRE_FILE = 'models/fre_network_7.pth'
+POLICY_FILE = 'models/model_7.pth'
+
+print('REPLAY_BUFFER_FILE:', REPLAY_BUFFER_FILE)
+print('FRE_FILE:', FRE_FILE)
+print('POLICY_FILE:', POLICY_FILE)
+
 
 
 
@@ -54,6 +62,8 @@ print('MODEL_SAVE_FOLDER:', MODEL_SAVE_FOLDER)
 
 
 
+
+
 def train_on_new_states(
     env: TorchWrapper, 
     model: ActorCriticContinuous, 
@@ -75,8 +85,8 @@ def train_on_new_states(
     z, _ = reward_generator.get_z_from_anchors(anchors, anchors_rewards, pad_mask)
 
     trajectory, _ = collect_trajectories(env, z, model, n_steps=num_policy_steps, reward_generator=reward_generator)
-    shuffled_trajectory = shufffle_trajectory(trajectory)
     for _  in range(4):
+        shuffled_trajectory = shufffle_trajectory(trajectory)
         entropy = ppo_optimization(shuffled_trajectory, model, optimizer, epochs=1, batch_size=512)
         
     avg_reward = trajectory['rewards'][len(trajectory['rewards'])//num_envs-1::len(trajectory['rewards'])//num_envs].mean()
@@ -253,7 +263,7 @@ def plot_logs():
     axs[2, 2].set_ylim([-X2_RANGE, X2_RANGE])
     axs[2, 2].set_title('Policy reaching')
         
-    plt.savefig(f"{LOGS_FOLDER}/epoch_{epoch}.png")
+    plt.savefig(f"{LOGS_FOLDER}/losses.png")
 
 
 
@@ -279,7 +289,7 @@ print('[INFO] Env imported')
 
 
 fre_network = FRENetwork(obs_len=2)
-# fre_network.load_state_dict(torch.load('models/fre_network_flexible.pth'))
+fre_network.load_state_dict(torch.load(FRE_FILE))
 
 reward_generator = RewardGenerator(
     obs_dim=2,
@@ -288,22 +298,17 @@ reward_generator = RewardGenerator(
     max_num_anchors=MAX_NUM_ANCHORS,
     from_buffer=True
 )
-# reward_generator.states_buffer = torch.rand((33, 1000, 2)) * 0.6 - 0.3
-# empericial_mean, empericial_std = reward_generator.estimate_mean_std(steps=1000)
 
-# x = torch.rand((1, 10000, 2)) * 0.6 - 0.3
-# x = x * STATE_SCALE
-# x = torch.round((x + 0.3) / 0.6 * 32)
 
-# x1_vals = np.linspace(-0.25, 0.25, 100) 
-# x2_vals = np.linspace(-0.175, 0.175, 100)
-# X1, X2 = np.meshgrid(x1_vals, x2_vals)
-# state_x12 = np.column_stack((X1.ravel(), X2.ravel()))
-# x = torch.tensor(state_x12, dtype=torch.float32) * STATE_SCALE
-# x = x.reshape(1, -1, 2)
+reward_generator.new_states_buffer = torch.load(REPLAY_BUFFER_FILE)['new_states_buffer']
 
-# reward_generator.update_new_states_buffer(x)
-# reward_generator.update_states_buffer    (x)
+if reward_generator.new_states_buffer is not None:
+    resampler = RNDResampling()
+    dataset = reward_generator.new_states_buffer[:, -1, :].reshape(-1, 2)
+    resampler_losses = resampler.fit(dataset, epochs=1000)
+    resampling_weights = resampler.get_resampling_weights(dataset)
+    reward_generator.resampling_weights = resampling_weights
+
 
 
 model = ActorCriticContinuous(
@@ -313,6 +318,7 @@ model = ActorCriticContinuous(
     actor_hidden_layers=[512, 512, 512, 512],
     critic_hidden_layers=[512, 512, 512, 512]
 ).to(device)
+model.load_state_dict(torch.load(POLICY_FILE))
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
@@ -334,149 +340,43 @@ training_steps = 0
 
 
 
-
-
-
-for epoch in tqdm(range(100)):
-    
-    ##### Explore new states using random actions ##################################################################
-    from_prior = True if (reward_generator.new_states_buffer is None) else False
-    print(f'Getting new states... from_prior={from_prior}')
-    trajectory, random_states, get_new_states_info = get_new_states(
-        5,
-        env, model, reward_generator, 
-        num_policy_steps=EPISODE_LENGTH, 
-        num_random_steps=NUM_RANDOM_STEPS, 
-        from_prior=from_prior
+##### train the VAE: ###########################################################################################
+print('VAE training...')
+for _ in tqdm(range(10), desc='VAE training', leave=False):
+    vae_loss_dict = reward_generator.train_step_VAE(
+        batch_size=512,
+        min_num_anchors=MIN_NUM_ANCHORS,
+        max_num_anchors=MAX_NUM_ANCHORS,
+        from_new_states=True,
+        num_states=64,
+        non_anchor_coef=0.5,
     )
-    
-    
-    if reward_generator.new_states_buffer is not None:
-        potential_targets = random_states[:, -1, :2]
-        old_targets = reward_generator.new_states_buffer[:, -1, :2]
-        
-        _, filter_info = filter_only_new_states(potential_targets, old_targets, steps=2000)
-        to_keep = filter_info['new_score'] > 0.8
-    
-        parcoured_states_and_random_states = torch.concat(
-            (
-                trajectory['states'].reshape(-1, EPISODE_LENGTH, 4)[to_keep],
-                random_states.reshape(-1, NUM_RANDOM_STEPS, 4)[to_keep]
-            ),
-            dim=1
-        )
-    
-    
-    else:
-        parcoured_states_and_random_states = torch.concat(
-            (
-                trajectory['states'].reshape(-1, EPISODE_LENGTH, 4),
-                random_states.reshape(-1, NUM_RANDOM_STEPS, 4)
-            ),
-            dim=1
-        )
+    vae_loss.append(vae_loss_dict['loss'])
+    vae_kl_loss.append(vae_loss_dict['kl_loss'])    
+plot_logs()
+
+os.system('nvidia-smi')
+torch.save(reward_generator.fre_network.state_dict(), f"{MODEL_SAVE_FOLDER}/fre_network.pth")
 
 
-    if reward_generator.new_states_buffer is not None:
-        viz_random_state = reward_generator.new_states_buffer[:, -1, :2].reshape(-1, 2)
-    viz_policy_reaches = trajectory['states'].reshape(-1, EPISODE_LENGTH, 4)[:, -1, :2].reshape(-1, 2)
-    
-    
-    # reward_generator.update_states_buffer(parcoured_states_and_random_states)
-    reward_generator.update_new_states_buffer(parcoured_states_and_random_states[..., :2])
-    # reward_generator.new_states_buffer = parcoured_states_and_random_states[..., :2]
-    
-    
-    resampler = RNDResampling()
-    dataset = reward_generator.new_states_buffer[:, -1, :].reshape(-1, 2)
-    resampler_losses = resampler.fit(dataset, epochs=1000)
-    resampling_weights = resampler.get_resampling_weights(dataset)
-    reward_generator.resampling_weights = resampling_weights
-    
-    
-    # Remove the random first please:
-    # if training_steps == 1:
-    #     reward_generator.new_states_buffer = reward_generator.new_states_buffer[640:]
-    
-    
-    # break
-    
 
-    viz_old_state = viz_new_state
-    # viz_new_state = trajectory['states'][..., :2].reshape(-1, 2)
-    viz_new_state = reward_generator.new_states_buffer[:, -1, :2].reshape(-1, 2)
-    # viz_random_state = random_states[..., :2].reshape(-1, 2)
+##### Train the policy on new states (Exploration) #############################################################
+print('Policy training on new states...')
 
-    # break
-    # continue
-    
-    plot_logs()
+model.action_var.data = torch.full((model.action_dim,), model.action_std**2, requires_grad=True, device=device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
 
+anchors_list = []
+for step in tqdm(range(10), desc='Policy training', leave=False):
+    avg_reward_1, entropy_1, info = train_on_new_states(env, model, optimizer, reward_generator, num_policy_steps=EPISODE_LENGTH)
+    anchors_list.append(info['anchors'])
+    rewards_list_1.append(avg_reward_1)
+    entropies_1.append(entropy_1)
     
-    
-    ##### train the VAE: ###########################################################################################
-    print('VAE training...')
-    for _ in tqdm(range(2000), desc='VAE training', leave=False):
-        vae_loss_dict = reward_generator.train_step_VAE(
-            batch_size=512,
-            min_num_anchors=MIN_NUM_ANCHORS,
-            max_num_anchors=MAX_NUM_ANCHORS,
-            from_new_states=True,
-            num_states=64,
-            non_anchor_coef=0.5,
-        )
-        vae_loss.append(vae_loss_dict['loss'])
-        vae_kl_loss.append(vae_loss_dict['kl_loss'])    
-    plot_logs()
+    if step % 1 == 0:
+        os.makedirs(f"{MODEL_SAVE_FOLDER}/step_{step}")
+        torch.save(model.state_dict(), f"{MODEL_SAVE_FOLDER}/step_{step}/model.pth")
+        plot_logs()
 
 
-    # ##### Estimate the emperical means and std to train the policy: ################################################
-    # print('Mean and std estimation...')
-    # empericial_mean, empericial_std = reward_generator.estimate_mean_std(steps=1000)    
-    
-    
-    
-    ##### Train the policy on new states (Exploration) #############################################################
-    print('Policy training on new states...')
-    
-    model.action_var.data = torch.full((model.action_dim,), model.action_std**2, requires_grad=True, device=device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
-
-    anchors_list = []
-    for _ in tqdm(range(50), desc='Policy training', leave=False):
-        avg_reward_1, entropy_1, info = train_on_new_states(env, model, optimizer, reward_generator, num_policy_steps=EPISODE_LENGTH)
-        anchors_list.append(info['anchors'])
-        rewards_list_1.append(avg_reward_1)
-        entropies_1.append(entropy_1)
-    plot_logs()
-    
-    
-    
-    # ##### Train the policy on learned reward functions (Rehearsal) #################################################
-    # print('Policy rehearsal...')
-    # for _ in range(10):
-    #     avg_reward_2, entropy_2 = policy_rehearsal(env, model, reward_generator, num_policy_steps=EPISODE_LENGTH)
-    #     rewards_list_2.append(avg_reward_2)
-    #     entropies_2.append(entropy_2)
-        
-    
-
-    # states_buffer_history.append((reward_generator.states_buff    er, reward_generator.new_states_buffer, filtered_new))
-
-    # break
-    
-    
-    training_steps += 1
-    
-    
-    
-    
-    os.makedirs(f"{MODEL_SAVE_FOLDER}/epoch_{epoch}")
-    torch.save(reward_generator.fre_network.state_dict(), f"{MODEL_SAVE_FOLDER}/epoch_{epoch}/fre_network.pth")
-    torch.save(reward_generator.optimimizer.state_dict(), f"{MODEL_SAVE_FOLDER}/epoch_{epoch}/fre_network_optimizer.pth")
-    torch.save(model.state_dict(), f"{MODEL_SAVE_FOLDER}/epoch_{epoch}/model.pth")
-    torch.save(optimizer.state_dict(), f"{MODEL_SAVE_FOLDER}/epoch_{epoch}/optimizer.pth")
-    torch.save({'new_states_buffer': reward_generator.new_states_buffer}, f"{MODEL_SAVE_FOLDER}/new_states_buffer.pth")
-
-
-    os.system('nvidia-smi')
+os.system('nvidia-smi')
